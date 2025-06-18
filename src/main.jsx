4062391ebc855550a42d21055c976324239671dd
pooja-agale <agalepooja38@gmail.com>
2025-06-18 16:20:42 +0530

    make some chanesss

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 //import { GoogleOAuthProvider } from "@react-oauth/google";
 
 //const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
@@ -11,9 +11,9 @@ import { BrowserRouter } from "react-router-dom";
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* <GoogleOAuthProvider clientId={clientId}> */}
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     {/* </GoogleOAuthProvider> */}
   </React.StrictMode>
 );
